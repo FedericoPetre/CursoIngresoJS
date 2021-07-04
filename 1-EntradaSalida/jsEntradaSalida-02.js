bcb6aabf50d8b8729417bf3b0ff6bb38c7486de4
FedericoPetre <federicopetre22@gmail.com>
2021-07-04 14:24:02 -0300

    Realicé el segundo ejercicio con prompt

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,9 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+	nombre = prompt("Ingrese su nombre"); // aquí uso prompt para ingresar por teclado el nombre
+
+	alert(nombre);
 }
 
