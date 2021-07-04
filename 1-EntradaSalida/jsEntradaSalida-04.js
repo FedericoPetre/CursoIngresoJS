04df9728e2a71dee42bebb16c1958b836e6bf0bb
FedericoPetre <federicopetre22@gmail.com>
2021-07-04 17:06:50 -0300

    realice los ejecicios 3, 4 y 5 tratando de comentar bien claro que es lo que hice

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,13 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	// aquí defino la variable nombre, en la cual se guardará el valor nombre que ingrese el usuario
+	var nombre;
+
+	// guardo en la variable nombre, el texto que ingreso el usuario dentro de la variable prompt	
+	nombre = prompt("Ingrese su nombre aquí");
+
+	// copio el nombre que tengo guardado dentro de la variable nombre en la caja de texto embebida en la página
+	document.getElementById("txtIdNombre").value = nombre;
 }
 
--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -3,6 +3,12 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+	let nombre; // aquí guardo un espacio en memoria para el texto "nombre" que ingrese el usuario
+	let edad; // aquí guardo un espacio en memoria para el numero "edad" que ingrese el usuario
+
+	nombre = document.getElementById("txtIdNombre").value; // aquí guardo en la variable nombre, el texto que ingresó el usuario en el cuadro enbebido de la página
+	edad = document.getElementById("txtIdEdad").value; // aquí guardo en la variable edad, el numero que representa la edad que el usuario ingreso en el cuadro embebido de la página
+
+	alert("Usted se llama " + nombre + " y tiene " + edad + " años"); // aquí devuelvo la oración con los datos concatenados que ingreso el usuario en cada cuadro embebido de la página
 }
 
